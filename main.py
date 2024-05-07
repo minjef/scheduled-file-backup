14a927ab57e628f3cef76a3ef4e4b6c138141167
minjef <minjef722@gmail.com>
2024-05-07 14:23:42 -0400

    added a file backup that runs on schedule

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+
+def main():
+
+    source_dir = "C:/Users/minje/OneDrive/Pictures/Screenshots"
+    destination_dir = "C:/Users/minje/OneDrive/Desktop/backup"
+
+    schedule.every().day.at("13:50").do(lambda : copy_folder_to_directory(source_dir, destination_dir))
+
+
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir= os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in {dest}")
+
+
+main()
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
